bfef63921165c5fedb86f1805c56e674e0754bc9
MohammadR Fekri <fekri.m@icloud.com>
2017-11-23 10:23:45 +0330

    fix-up: added one character shortname for friday.

--- app/Helpers/DaysOfWeek.php
+++ app/Helpers/DaysOfWeek.php
@@ -29,13 +29,14 @@ class DaysOfWeek
     public static function collection()
     {
         return collect([
-            new self(0, 'شنبه',      'ش'),
-            new self(1, 'یک‌شنبه',    'ی'),
-            new self(2, 'دو‌شنبه',    'د'),
-            new self(3, 'سه‌شنبه',    'س'),
-            new self(4, 'چهار‌شنبه',  'چ'),
-            new self(5, 'پنج‌شنبه',   'پ'),
-            new self(6, 'جمعه',      'جم'),
+            new self(0, 'شنبه', 'ش'),
+            new self(1, 'یک‌شنبه', 'ی'),
+            new self(2, 'دو‌شنبه', 'د'),
+            new self(3, 'سه‌شنبه', 'س'),
+            new self(4, 'چهار‌شنبه', 'چ'),
+            new self(5, 'پنج‌شنبه', 'پ'),
+            new self(6, 'جمعه', 'جم'),
+            new self(6, 'جمعه', 'ج'),
         ]);
     }
 }
