c1d3c543f4c8056452e98fd1388a57ca8e6c089b
MohammadR Fekri <fekri.m@icloud.com>
2017-12-14 20:47:18 +0330

    removed tailwinds completely

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
-let mix = require('laravel-mix'),
-    tailwindcss = require('tailwindcss');
+let mix = require('laravel-mix');
 
 
 /*
@@ -14,7 +13,4 @@ let mix = require('laravel-mix'),
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css').options({
-        processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js'),],
-    });
+   .sass('resources/assets/sass/app.scss', 'public/css');
